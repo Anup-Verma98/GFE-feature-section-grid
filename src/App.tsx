25290e4b943716e2dc86cef0b27274349326bfcf
anup verma <anup.verma@appinventiv.com>
2025-07-08 12:26:27 +0530

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  /**
+    Premium abstract images
+    
+    Easy access to top quality images
+    
+    In a world where storytelling constantly evolves, we lead with groundbreaking images designed for your presentation excellence.
+    
+    - Infinite Download: Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.
+    - Purely Handcrafted: No AI, no generic images. Crafted from various chemicals, fabrics, clouds, or even particles as small as dust.
+    - All Are Under licensed: The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy (such as paintings or prints on paper).
+    - Cancel Anytime: Subscribe at your own pace, and cancel when you feel it's enough.
+    - Empowering For Team: We support multiple seats at once, requiring only a single payment.
+    - No Limitations: Use as many as you want, from Dribbble presentations to PowerPoint presentations. 
+   */
+
+  return <>Hello World!!</>;
+}
+
+export default App;
